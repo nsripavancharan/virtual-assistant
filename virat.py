f725c399387f79032ee999cfb5de7f601e0d5197
nsripavancharan <nunepavancharan@gmail.com>
2025-02-01 18:33:55 +0530

    Update virat.py

--- virat.py
+++ virat.py
@@ -1,8 +1,9 @@
 import speech_recognition as aa
-import pyttsx3
-import pywhatkit
+import pyttsx3 #The pyttsx3 module is used for offline text-to-speech (TTS) conversion. Unlike gTTS, it doesn't require an internet connection.
+import pywhatkit #The pywhatkit module is used for automation, such as sending WhatsApp messages, playing YouTube videos, and performing Google searches.
 import datetime
 import wikipedia
+import webbrowser
 listener = aa.Recognizer()
 machine =pyttsx3.init()
 
@@ -44,7 +45,22 @@ def play_virat():
             talk("i am fine, how about you")
         elif 'what is your name' in instruction:
             talk("I am virat")
+        elif 'explain' in instruction:
+            human = instruction.replace('explain', "")
+            info = wikipedia.summary(human, 1)
+            print(info)
+            talk(info)
+        elif "search" in instruction:
+            a=instruction.replace('search',"")
+            pywhatkit.search(a)
 
+        elif "send" in instruction:
+            name=instruction.replace('send',"")
+            msg="hello ,this is assistant"
+            pywhatkit.sendwhatmsg_instantly("+919154715354",msg ) 
+        elif "open" in instruction:
+            web=instruction.replace('open',"")
+            webbrowser.open(web)
         elif 'who is' in instruction:
             human=instruction.replace('who is', " ")
             info=wikipedia.summary(human ,1)
